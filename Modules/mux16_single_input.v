a6936535fd6acc0aaab237aa9c2bdeaeb27511b2
Dom324 <57506340+Dom324@users.noreply.github.com>
2020-10-18 19:42:00 +0200

    Add files via upload

---
+++ Modules/mux16_single_input.v
@@ -0,0 +1,29 @@
+module mux16_single_input
+  (input [3:0] select,
+  input  [15:0] a,
+  output out);
+
+  always begin
+
+    case(select)
+
+    4'b0000: out = a[0];
+	  4'b0001: out = a[1];
+	  4'b0010: out = a[2];
+	  4'b0011: out = a[3];
+	  4'b0100: out = a[4];
+	  4'b0101: out = a[5];
+	  4'b0110: out = a[6];
+	  4'b0111: out = a[7];
+    4'b1000: out = a[8];
+    4'b1001: out = a[9];
+    4'b1010: out = a[10];
+    4'b1011: out = a[11];
+    4'b1100: out = a[12];
+    4'b1101: out = a[13];
+    4'b1110: out = a[14];
+    4'b1111: out = a[15];
+
+    endcase
+  end
+endmodule
---
+++ Modules/mux4.sv
@@ -0,0 +1,18 @@
+module mux4
+ #(parameter width = 1)
+  (input logic[1:0] select,
+  input logic  [width-1:0] a, b, c, d,
+  output logic [width-1:0] out);
+  
+  always_comb begin
+  
+    case(select)
+  
+      2'b00: out = a;
+	  2'b01: out = b;
+	  2'b10: out = c;
+	  2'b11: out = d;
+	  
+    endcase
+  end
+endmodule
