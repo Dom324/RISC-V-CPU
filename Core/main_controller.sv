74e9dc04d43f949294d650ee6e677b6ea6b54295
Dom324 <dominikliberda@seznam.cz>
2021-02-25 14:17:25 +0100

    Vylepseno fetchovani instrukci

--- Core/main_controller.sv
+++ Core/main_controller.sv
@@ -11,37 +11,69 @@ module main_controller(
   output logic [31:0] nextPC,
   output logic [31:0] instr_fetch_exec,
   output logic fetch_enable,
-  output logic stall_pc,
   output logic we_reg,
-  output logic fetch_valid_exec
+  output logic fetch_valid_exec,
+  output logic stall_pc,
+  input logic stall_debug
   );
 
-  logic fetch_valid_buffer, fetch_valid_buffer_next, decoder_stall_buffer;
-  logic [31:0] instr_fetch_buffer, instr_fetch_buffer_next;
+  logic fetch_valid_buffer;
+  logic [31:0] instr_fetch_buffer;
   logic [31:0] PC_reg, nextPC_reg;
 
-  logic stall_reg, stall_pc_delayed;
+  logic stall_reg;
 
-always_ff @ (posedge CLK, negedge resetn) begin
+  logic new_instr, instr_executed;
+
+  logic fetch_enable_reg;
+
+always_ff @ (posedge CLK) begin
+
+  if(!resetn) fetch_enable_reg <= 1;
+  else begin
+
+    if(new_instr) begin
+      if(instr_executed) fetch_enable_reg <= 1;
+      else fetch_enable_reg <= 0;
+    end else if(instr_executed) fetch_enable_reg <= 1;
+
+  end
+
+end
+
+always_comb begin
+
+  fetch_enable = fetch_enable_reg;
+
+  if(new_instr) begin
+    if(instr_executed) fetch_enable = 1;
+    else fetch_enable = 0;
+  end else if(instr_executed) fetch_enable = 1;
+
+end
+
+always_ff @ (posedge CLK) begin
 
   if(!resetn) begin
     fetch_valid_buffer <= 0;
     instr_fetch_buffer <= 0;
+    //stall_debug_buffer <= 0;
   end
   else begin
 
-    fetch_valid_buffer <= fetch_valid_buffer_next;
-    instr_fetch_buffer <= instr_fetch_buffer_next;
-    decoder_stall_buffer <= decoder_stall & fetch_valid_exec;
+    instr_fetch_buffer <= instr_fetch_exec;
+
+    if(instr_executed) fetch_valid_buffer <= 0;
+    else begin
+
+      if(new_instr) fetch_valid_buffer <= 1;
+      else fetch_valid_buffer <= fetch_valid_buffer;
 
   end
+
 end
 
-
-always_comb begin
-
-  fetch_valid_buffer_next = fetch_valid_buffer;
-  instr_fetch_buffer_next = instr_fetch_buffer;
+/*fetch_valid_buffer_next = fetch_valid_buffer;
 
 if(fetch_valid) begin     //instrukce z icache je valid
 
@@ -63,15 +95,26 @@ else begin
 
   if(stall_pc) begin
 
-    fetch_valid_buffer_next = fetch_valid_buffer;
-    instr_fetch_buffer_next = instr_fetch_buffer;
+    fetch_valid_buffer_next = fetch_valid_exec;
+    instr_fetch_buffer_next = instr_fetch_exec;
 
   end
   else begin
     fetch_valid_buffer_next = 0;
-  end
+  end*/
 
 end
+
+always_comb begin
+
+  instr_executed = 0;
+
+  if(fetch_valid_exec) begin
+
+    if(stall_pc) instr_executed = 0;
+    else instr_executed = 1;
+
+  end
 
 end
 
@@ -79,6 +122,7 @@ always_comb begin
 
   fetch_valid_exec = fetch_valid_buffer;
   instr_fetch_exec = instr_fetch_buffer;
+  new_instr = 0;
 
     if(fetch_valid) begin     //instrukce z icache je valid
 
@@ -87,6 +131,8 @@ always_comb begin
         fetch_valid_exec = fetch_valid;
         instr_fetch_exec = instr_fetch;
 
+        new_instr = 1;
+
       end
 
     end
@@ -94,52 +140,42 @@ always_comb begin
 end
 
 
-always_ff @ (posedge CLK, negedge resetn) begin
+always_ff @ (posedge CLK) begin
 
   if(!resetn) begin
-    PC_reg <= 32'h00000FFC;
-    nextPC_reg <= 32'h00001000;
+    PC_reg <= 32'hFFFFFFFC;
+    nextPC_reg <= 32'h00000000;
   end
   else begin
 
     //if(!stall_pc & (nextPC <= 32'h00000008))
       //PC <= nextPC;
 
+    if(nextPC <= 32'h0000008C)begin
     PC_reg <= PC;
     nextPC_reg <= nextPC;
+    end
 
   end
 
 end
 
-  assign PC = stall_pc_delayed ? PC_reg : nextPC_reg;
-  assign nextPC = stall_pc_delayed ? nextPC_reg : PCmux;
+  /*assign PC = stall_pc_delayed ? PC_reg : nextPC_reg;
+  assign nextPC = stall_pc_delayed ? nextPC_reg : PCmux;*/
 
-/*always_comb begin
+always_comb begin
 
-  if(stall_pc_delayed) PC = PC_reg;
-  else PC = nextPC_reg;
+  if(new_instr) PC = nextPC_reg;
+  else PC = PC_reg;
 
-  if(stall_pc_delayed) nextPC = nextPC_reg;
-  else nextPC = PCmux;
+  if(new_instr) nextPC = PCmux;
+  else nextPC = nextPC_reg;
 
-end*/
+end
 
   //logika PC
 
   //stall logika
-
-always_comb begin
-
-  if(fetch_valid_exec) begin
-
-    if(decoder_stall_buffer) stall_pc_delayed = 1;
-    else stall_pc_delayed = 0;
-
-  end else stall_pc_delayed = 1;
-
-end
-
 always_comb begin
 
   if(stall_reg == 1) we_reg = 0;
@@ -151,22 +187,23 @@ always_comb begin
 
   if(fetch_valid_exec) begin
 
-    if(decoder_stall) begin
+    if(stall_debug) begin
       stall_pc = 1;
       stall_reg = 1;
-      fetch_enable = 0;
+    end
+    else if(decoder_stall) begin
+      stall_pc = 1;
+      stall_reg = 1;
     end
     else begin
       stall_pc = 0;
       stall_reg = 0;
-      fetch_enable = 1;
     end
 
   end
   else begin
     stall_pc = 1;
     stall_reg = 1;
-    fetch_enable = 1;
   end
 
 end
