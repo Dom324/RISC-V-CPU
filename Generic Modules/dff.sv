5004cd9a2b9384ff1c6a078b2abc83cdd82c30c0
Dom324 <dominikliberda@seznam.cz>
2021-03-04 23:40:15 +0100

    Mala chyba

--- Generic Modules/dff.sv
+++ Generic Modules/dff.sv
@@ -1,12 +1,12 @@
 module dff(
-  input logic  clk, data, reset,
+  input logic  clk, data, resetn,
   output logic q
 );
 
 
 always_ff @ (posedge clk) begin
 
-  if(!reset)
+  if(resetn)
     q <= data;
   else
     q <= 0;
