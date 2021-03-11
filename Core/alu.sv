d8b59042e0287e9e7df02ad63bd8e2104ba1dfc1
Dom324 <dominikliberda@seznam.cz>
2021-03-11 10:34:03 +0100

    Prejmenovani

--- Core/alu.sv
+++ Core/alu.sv
@@ -1,6 +1,6 @@
 module alu(
   input logic  [31:0] a, b,
-  input logic [2:0] funct3,
+  input logic [2:0] aluOp,
   input logic [6:0] funct7,
   output logic [31:0] out
 );
@@ -11,7 +11,7 @@ always_comb begin
 //defaultni hodnoty
   out = 0;
 
-  case(funct3)
+  case(aluOp)
 
     3'b000:	begin
 
